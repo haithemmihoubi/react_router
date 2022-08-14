23cd82cc3484dabf6c3087fe65e779441a65c9a5
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-08-14 01:37:11 +0100

    link router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
-import { Route,Routes,BrowserRouter as Router } from "react-router-dom";
+import {Route, Routes, BrowserRouter as Router, Link} from "react-router-dom";
 import {Home} from "./pages/Home";
+import {Menu} from "./pages/Menu";
+import {Contact} from "./pages/contact";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-            <Route path="/" element={<Home></Home> } />
-          </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/menu">Menu</Link>
+                        <Link to="/contact">Contact</Link>
+                    </nav>
+
+                </div>
+
+
+                <Routes>
+                    <Route path="/" element={<Home></Home>}/>
+                    <Route path="/menu" element={<Menu/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Menu.js
@@ -0,0 +1,8 @@
+export const Menu = () => {
+    return (
+        <div>
+            <h1>Menu Page</h1>
+        </div>
+    );
+
+}
